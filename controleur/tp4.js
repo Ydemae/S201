71e7225631430d115199955705541d8b3b99955a
Ydemae <113693952+Ydemae@users.noreply.github.com>
2023-04-17 11:11:07 +0200

    first

---
+++ controleur/tp4.js
@@ -0,0 +1,21 @@
+import { vueTp4 } from "../controleur/class_tp4";
+vueTp4.init({ divFormulaire: document.querySelector('[id=div_formulaire]'),
+    divRecap: document.querySelector('[id=div_recap]'),
+    divFormBoutons: document.querySelector('[id=div_formulaire_boutons]'),
+    radioMadame: document.querySelector('[id=radio_madame]'),
+    radioMonsieur: document.querySelector('[id=radio_monsieur]'),
+    edtNom: document.querySelector('[id=edt_nom]'),
+    edtPrenom: document.querySelector('[id=edt_prenom]'),
+    edtDtNais: document.querySelector('[id=edt_dtnais]'),
+    listeDiplome: document.querySelector('[id=select_diplome]'),
+    edtExperience: document.querySelector('[id=edt_experience]'),
+    chkWindows: document.querySelector('[id=chk_windows]'),
+    chkLinux: document.querySelector('[id=chk_linux]'),
+    chkAutre: document.querySelector('[id=chk_autre]'),
+    edtAutre: document.querySelector('[id=edt_autre]'),
+    edtCommentaire: document.querySelector('[id=edt_commentaire]'),
+    listeRecap: document.querySelector('[id=select_recap]'),
+    btnValider: document.querySelector('[id=btn_valider]'),
+    btnCorriger: document.querySelector('[id=btn_corriger]')
+});
+//# sourceMappingURL=tp4.js.map
